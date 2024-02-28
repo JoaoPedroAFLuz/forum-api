aba356b9df47159deafb9ff82306d2298c283d4a
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-02-27 21:09:24 -0300

    criar entidade de id

--- src/domain/core/entities/entity.ts
+++ src/domain/core/entities/entity.ts
@@ -1,11 +1,11 @@
-import { randomUUID } from 'node:crypto';
+import { UniqueEntityId } from './unique-entity-id';
 
 export class Entity<T> {
-  private _id: string;
+  private _id: UniqueEntityId;
   protected props: T;
 
   constructor(props: any, id?: string) {
-    this._id = id ?? randomUUID();
+    this._id = new UniqueEntityId(id);
     this.props = props;
   }
 
