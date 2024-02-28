9b48c9e8a01b11d97d423043fd7e2a011128fb7c
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-02-28 12:26:55 -0300

    feat: teste unitario do use case de criacao de pergunta

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { CreateQuestionUseCase } from './create-question';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CreateQuestionUseCase;
+
+describe('Create a Question', () => {
+  beforeAll(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
+  });
+
+  it('should be able to create a question', async () => {
+    const { question } = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'Conteúdo da pergunta',
+    });
+
+    expect(question.id).toBeTruthy();
+    expect(question.title).toEqual('Nova pergunta');
+    expect(question.content).toEqual('Conteúdo da pergunta');
+  });
+});
