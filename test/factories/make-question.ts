4e36cf90b9dabdb6f0ac43bb70aff9853ea22a54
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-02-28 13:29:55 -0300

    refactor: usar dados ficticios na factory de perguntas

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,22 @@
+import { faker } from '@faker-js/faker';
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question';
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  return Question.create({
-    authorId: new UniqueEntityId('1'),
-    title: 'Example title',
-    slug: Slug.create('example-slug'),
-    content: 'Example content',
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  return Question.create(
+    {
+      authorId: new UniqueEntityId('1'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 }
